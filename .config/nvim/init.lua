df16347921dd008ce1675f00b10f374fff13b5f2
Bjarke Jacobsen Julius <bjarke.julius@gmail.com>
2025-05-25 16:23:31 +0200

    added alot

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,7 @@
 require("config.lazy")
 
+local tabSpaces = 4
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.autochdir = true
@@ -9,6 +11,12 @@ vim.opt.cursorline = true
 vim.opt.mousehide = true
 vim.opt.scrolloff = 5
 vim.opt.smarttab = true
-vim.opt.tabstop = 4
+vim.opt.tabstop = tabSpaces
 vim.opt.splitright = true
 vim.opt.swapfile = false
+vim.opt.expandtab = true
+vim.opt.shiftwidth = tabSpaces
+
+vim.g.mapleader = ","
+
+vim.keymap.set("n", "<Leader>r", ":w|!gcc %;./a.out <Enter>")
